c1be555a2e931c8fcb5c8481bafb82731ea2c417
Givikap <kaitosekiya@outlook.com>
2024-04-06 14:03:13 -0500

    Update structure and add .gitignore's

--- server/server.py
+++ server/server.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def defualt():
     return "<h1>DEFAULT DEVELOPMENT SERVER.</h1>"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
